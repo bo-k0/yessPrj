69f1eef826728fa6a248f26d9c183eed13af3028
JIwoonwoo <wcw703@naver.com>
2022-12-27 09:14:18 +0900

    how to yess 어노테이션 추가

--- yessPrj/src/main/java/com/kh/yess/howtoYeSS/dao/HowToYeSSDaoImpl.java
+++ yessPrj/src/main/java/com/kh/yess/howtoYeSS/dao/HowToYeSSDaoImpl.java
@@ -1,5 +1,8 @@
 package com.kh.yess.howtoYeSS.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class HowToYeSSDaoImpl implements HowToYeSSDao{
 
 }
--- yessPrj/src/main/java/com/kh/yess/howtoYeSS/service/HowToYeSSServiceImpl.java
+++ yessPrj/src/main/java/com/kh/yess/howtoYeSS/service/HowToYeSSServiceImpl.java
@@ -1,5 +1,8 @@
 package com.kh.yess.howtoYeSS.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HowToYeSSServiceImpl implements HowToYeSSService{
 
 }
